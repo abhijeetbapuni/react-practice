60a3165bd4b7955ae2a7c0f0dac02fb6bd3363dc
Abhijeet Maharana <abhijeetmaharana10@gmail.com>
2022-11-21 20:55:56 +0530

    package json

--- src/components3/Users/AddUser.js
+++ src/components3/Users/AddUser.js
@@ -44,7 +44,7 @@ const AddUser = (props) => {
   };
 
   return (
-    <React.Fragment>
+    <>
       {error && (
         <ErrorModal
           title={error.title}
@@ -71,7 +71,7 @@ const AddUser = (props) => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </React.Fragment>
+    </>
   );
 };
 
