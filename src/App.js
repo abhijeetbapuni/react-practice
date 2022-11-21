0574cec6a75b6371664ec3385b3099135a3cb92d
Abhijeet Maharana <abhijeetmaharana10@gmail.com>
2022-11-22 00:32:52 +0530

    refractoring to a input component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Home from "./components4/Home/Home";
 import MainHeader from "./components4/MainHeader/MainHeader";
 import AuthContext from "./store/auth-context";
 
+// React context is not optimized for high frequency changes
+//Only call react hooks in react functions i.e. react component functions and custom hooks
+//Only call react hooks at the top level, not inside in nested functions or block level statements
 function App() {
   const ctx = useContext(AuthContext);
   // don't use like this
