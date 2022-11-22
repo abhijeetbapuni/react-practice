11357b83a572acfd7566750442ba69eb152723e7
Abhijeet Maharana <abhijeetmaharana10@gmail.com>
2022-11-23 02:53:08 +0530

    configuring custom hooks to make it more genric

--- src/components/hooks/use-counter.js
+++ src/components/hooks/use-counter.js
@@ -3,12 +3,16 @@ import { useEffect, useState } from "react";
 //always create hooks with function name starting with hook
 //hook shares the logic and states accorss components
 //each time you use this hook in components, the states used here are registered for that component
-const useCounter = () => {
+const useCounter = (forwards = true) => {
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter + 1);
+      if (forwards) {
+        setCounter((prevCounter) => prevCounter + 1);
+      } else {
+        setCounter((prevCounter) => prevCounter - 1);
+      }
     }, 1000);
 
     return () => clearInterval(interval);
