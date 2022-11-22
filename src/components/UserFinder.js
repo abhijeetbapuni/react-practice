09430fb6862924e0a278713c5235d9c7c69bd7d7
Abhijeet Maharana <abhijeetmaharana10@gmail.com>
2022-11-23 00:53:57 +0530

    used class based component with context api

--- src/components/UserFinder.js
+++ src/components/UserFinder.js
@@ -2,18 +2,14 @@ import { Fragment, Component } from "react";
 
 import Users from "./Users";
 import classes from "./UserFinder.module.css";
-
-const DUMMY_USERS = [
-  { id: "u1", name: "Max" },
-  { id: "u2", name: "Manuel" },
-  { id: "u3", name: "Julie" },
-];
+import UsersContext from "../store/users-context";
 
 class UserFinder extends Component {
+  static contextType = UsersContext; //only one context per component, then use this.context to access this context
   constructor() {
     super();
     this.state = {
-      filteredUsers: DUMMY_USERS,
+      filteredUsers: [], //can't access the context here, so access the context in componentDidMount
       searchTerm: "",
     };
   }
@@ -24,13 +20,13 @@ class UserFinder extends Component {
   //like useEffect with no dependencies and so once
   componentDidMount() {
     //send http request...
-    this.setState({ filteredUsers: DUMMY_USERS });
+    this.setState({ filteredUsers: this.context.users });
   }
   //like useEffect with dependencies
   componentDidUpdate(prevProps, prevState) {
     if (prevState.searchTerm !== this.state.searchTerm) {
       this.setState({
-        filteredUsers: DUMMY_USERS.filter((user) =>
+        filteredUsers: this.context.users.filter((user) =>
           user.name.includes(this.state.searchTerm)
         ),
       });
@@ -39,6 +35,7 @@ class UserFinder extends Component {
   render() {
     return (
       <Fragment>
+        {/* you can also wrap the component with consume like we did earlier */}
         <div className={classes.finder}>
           <input type="search" onChange={this.searchChangeHandler.bind(this)} />
         </div>
