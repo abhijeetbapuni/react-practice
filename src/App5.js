e1048a9806e0f3bc1845a64bf61ef8cd0e1836c7
Abhijeet Maharana <abhijeetmaharana10@gmail.com>
2022-11-23 21:38:54 +0530

    making meals app dynamic

--- src/App5.js
+++ src/App5.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 import Header from "./components5/Layout/Header";
 import Meals from "./components5/Meals/Meals";
 import Cart from "./components5/Cart/Cart";
-import CartProvider from "./store/CartProvider";
+import CartProvider from "./store5/CartProvider";
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
