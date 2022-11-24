7d53487398e72fe5f9b1860c02ec5a659414b110
Abhijeet Maharana <abhijeetmaharana10@gmail.com>
2022-11-24 06:18:12 +0530

    redux toolkit creating store

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,8 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { createStore } from "redux";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
 
-createSlice({
+const counterSlice = createSlice({
   name: "counter", //identifier//use any name you want
   initialState,
   reducers: {
@@ -22,39 +21,11 @@ createSlice({
   },
 });
 
-//state is by default undefined, so provide initial state for state
-const counterReducer = (state = initialState, action) => {
-  if (action.type === "increment") {
-    // state.counter++; //This works for never do this, always return a separate object and return all the keys as react doesn't merge the state, it overrides the state
-    return {
-      counter: state.counter + 1,
-      showCounter: state.showCounter,
-    };
-  }
-
-  if (action.type === "increase") {
-    return {
-      counter: state.counter + action.amount,
-      showCounter: state.showCounter,
-    };
-  }
-
-  if (action.type === "decrement") {
-    return {
-      counter: state.counter - 1,
-      showCounter: state.showCounter,
-    };
-  }
-  if (action.type === "toggle") {
-    return {
-      showCounter: !state.showCounter,
-      counter: state.counter,
-    };
-  }
-  return state;
-};
-
-const store = createStore(counterReducer);
+const store = configureStore({
+  //key name must be reducer
+  //if you have multiple reducers then pass in reducer like this {counter:counterSlice.reducer,...}
+  reducer: counterSlice.reducer,
+});
 
 export default store;
 
