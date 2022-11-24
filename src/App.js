74ba90c21403b0574f42b602b529e2e212c1b842
Abhijeet Maharana <abhijeetmaharana10@gmail.com>
2022-11-24 06:46:04 +0530

    reading and dispatching in other components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import { Provider } from "react-redux";
+import { Provider, useSelector } from "react-redux";
 import Counter from "./components/Counter";
 import store from "./store";
+import Header from "./components/Header";
+import Auth from "./components/Auth";
+import UserProfile from "./components/UserProfile";
 
 function App() {
+  const isAuth = useSelector((state) => state.authReducer.isAuthenticated);
   return (
-    // also we can import provider in index.js root level
-    <Provider store={store}>
+    <>
+      <Header />
+      {!isAuth && <Auth />}
+      {isAuth && <UserProfile />}
       <Counter />
-    </Provider>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import store from "./store";
 // import { AuthContextProvider } from "./store/auth-context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
   // <AuthContextProvider>
-  <App />
+  //better to wrap provider here
+  <Provider store={store}>
+    <App />
+  </Provider>
   // </AuthContextProvider>
   // </React.StrictMode>
 );
